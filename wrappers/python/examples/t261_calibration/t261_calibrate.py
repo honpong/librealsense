342434991c24a87165eb873401f035c8039868e9
Phillip Schmidt <phillip.schmidt@intel.com>
2019-08-22 15:36:37 -0700

    add convenience function to read calibration

--- wrappers/python/examples/t261_calibration/t261_calibrate.py
+++ wrappers/python/examples/t261_calibration/t261_calibrate.py
@@ -429,6 +429,23 @@ def reset_calibration():
         if tm2:
             tm2.reset_to_factory_calibration()
 
+def read_calibration():
+    ctx = rs.context()
+    devs = ctx.query_devices()
+    for dev in devs:
+        tm2 = dev.as_tm2()
+        if tm2:
+            sensors = tm2.query_sensors()
+            for sensor in sensors:
+                profiles = sensor.get_stream_profiles()
+                for profile in profiles:
+                    if profile.is_video_stream_profile():
+                        vp = profile.as_video_stream_profile()
+                        print(vp.get_intrinsics())
+                    elif profile.is_motion_stream_profile():
+                        vp = profile.as_motion_stream_profile()
+                        print(vp.get_motion_intrinsics())
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -437,11 +454,16 @@ if __name__ == "__main__":
     parser.add_argument('--extrinsics', default=False, help='calibrate extrinsics', action='store_true')
     parser.add_argument('--confirm', default=False, help='write calibration to device (w/o prompt)', action='store_true')
     parser.add_argument('--reset', default=False, help='reset calibration to factory default', action='store_true')
+    parser.add_argument('--read', default=False, help='reset calibration to factory default', action='store_true')
     args = parser.parse_args()
     tmp_folder = "tmp"
 
     if args.reset:
         reset_calibration()
+        sys.exit()
+
+    if args.read:
+        read_calibration()
         sys.exit()
 
     try:
