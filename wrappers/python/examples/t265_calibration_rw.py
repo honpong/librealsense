6d0cac18fb952fe0b9a3aab2cc975af4c4361111
Phillip Schmidt <phillip.schmidt@intel.com>
2019-08-21 17:30:30 -0700

    wip

--- wrappers/python/examples/t265_calibration_rw.py
+++ wrappers/python/examples/t265_calibration_rw.py
@@ -9,66 +9,53 @@
 
 import pyrealsense2 as rs
 
+def read_calibration():
+    global devs
+    global tm2
+
+    for dev in devs:
+        tm2 = dev.as_tm2()
+        if tm2:
+            sensors = tm2.query_sensors()
+            for sensor in sensors:
+                profiles = sensor.get_stream_profiles()
+                for profile in profiles:
+                    if profile.is_video_stream_profile():
+                        vp = profile.as_video_stream_profile()
+                        print(vp.get_intrinsics())
+                    elif profile.is_motion_stream_profile():
+                        vp = profile.as_motion_stream_profile()
+                        print(vp.get_motion_intrinsics())
+
+def write_calibration():
+    global devs
+    global tm2
+
+    fisheye_intrinsics = rs.intrinsics()
+    motion_intrinsics = rs.motion_device_intrinsic()
+
+    for dev in devs:
+        tm2 = dev.as_tm2()
+        if tm2:
+            tm2.set_intrinsics(1, fisheye_intrinsics)
+            tm2.set_intrinsics(2, fisheye_intrinsics)
+
+            tm2.set_motion_device_intrinsics(rs.stream.accel, motion_intrinsics)
+            tm2.set_motion_device_intrinsics(rs.stream.gyro, motion_intrinsics)
+
+            tm2.write_calibration()
+
 ctx = rs.context()
 
 devs = ctx.query_devices()
 
-tm2 = None
-for dev in devs:
-    tm2 = dev.as_tm2()
-    if tm2:
-        sensors = tm2.query_sensors()
-        for sensor in sensors:
-            profiles = sensor.get_stream_profiles()
-            for profile in profiles:
-                if profile.is_video_stream_profile():
-                    vp = profile.as_video_stream_profile()
-                    print(vp.get_intrinsics())
-                elif profile.is_motion_stream_profile():
-                    vp = profile.as_motion_stream_profile()
-                    print(vp.get_motion_intrinsics())
+read_calibration()
 
-# write
-fisheye_intrinsics = rs.intrinsics()
-motion_intrinsics = rs.motion_device_intrinsic()
-if tm2:
-    tm2.set_intrinsics(1, fisheye_intrinsics)
-    tm2.set_intrinsics(2, fisheye_intrinsics)
+write_calibration()
 
-    tm2.set_motion_device_intrinsics(rs.stream.accel, motion_intrinsics)
-    tm2.set_motion_device_intrinsics(rs.stream.gyro, motion_intrinsics)
-
-    tm2.write_calibration()
-
-# read 
-for dev in devs:
-    tm2 = dev.as_tm2()
-    if tm2:
-        sensors = tm2.query_sensors()
-        for sensor in sensors:
-            profiles = sensor.get_stream_profiles()
-            for profile in profiles:
-                if profile.is_video_stream_profile():
-                    vp = profile.as_video_stream_profile()
-                    print(vp.get_intrinsics())
-                elif profile.is_motion_stream_profile():
-                    vp = profile.as_motion_stream_profile()
-                    print(vp.get_motion_intrinsics())
+read_calibration()
 
 if tm2:
     tm2.reset_to_factory_calibration()
 
-# read
-for dev in devs:
-    tm2 = dev.as_tm2()
-    if tm2:
-        sensors = tm2.query_sensors()
-        for sensor in sensors:
-            profiles = sensor.get_stream_profiles()
-            for profile in profiles:
-                if profile.is_video_stream_profile():
-                    vp = profile.as_video_stream_profile()
-                    print(vp.get_intrinsics())
-                elif profile.is_motion_stream_profile():
-                    vp = profile.as_motion_stream_profile()
-                    print(vp.get_motion_intrinsics())
+read_calibration()
