0880ad804dcb3b09ce091d55ff765411676d51e8
Phillip Schmidt <phillip.schmidt@intel.com>
2019-08-30 16:10:57 -0700

    removed install instructions from script to avoid redundancy to readme

--- wrappers/python/examples/t261_calibration/t261_calibrate.py
+++ wrappers/python/examples/t261_calibration/t261_calibrate.py
@@ -4,24 +4,6 @@
 ## Copyright(c) 2019 Intel Corporation. All Rights Reserved.
 from __future__ import print_function  # Python 2/3 compatibility
 
-"""
-First, set up the virtual enviroment:
-
-$ apt-get install python3-venv  # install python3 built in venv support
-$ python3 -m venv py3librs      # create a virtual environment in pylibrs
-$ source py3librs/bin/activate  # activate the venv, do this from every terminal
-$ pip install opencv-python     # install opencv 4.1 in the venv
-$ pip install opencv-contrib-python     # install opencv 4.1 in the venv
-$ pip install pyrealsense2      # install librealsense python bindings
-$ pip install matplotlib        # install matplotlib in the venv
-$ pip install transformations   # install transformations in the venv
-
-Then, for every new terminal:
-
-$ source py3librs/bin/activate  # Activate the virtual environment
-$ python3 t265_aruco.py        # Run the example
-"""
-
 import pyrealsense2 as rs
 import cv2
 import numpy as np
