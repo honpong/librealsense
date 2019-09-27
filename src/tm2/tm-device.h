98d413085d5a210ed58cd28fcb48aabdda1e6f38
Hon Pong (Gary) Ho <hon.pong.ho@intel.com>
2019-09-27 18:03:50 +0800

    motion.h add origin change API

--- src/tm2/tm-device.h
+++ src/tm2/tm-device.h
@@ -100,6 +100,8 @@ namespace librealsense
         bool import_relocalization_map(const std::vector<uint8_t>& lmap_buf) const override;
         bool set_static_node(const std::string& guid, const float3& pos, const float4& orient_quat) const override;
         bool get_static_node(const std::string& guid, float3& pos, float4& orient_quat) const override;
+        bool change_pose_origin(const std::string& guid) const override;
+        bool change_pose_origin(uint32_t map_id) const  override;
 
         // Wheel odometer
         bool load_wheel_odometery_config(const std::vector<uint8_t>& odometry_config_buf) const override;
